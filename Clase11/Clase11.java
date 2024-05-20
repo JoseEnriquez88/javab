1a3e686a5af4b8f5a1c7f1b73ced8448ed3cca4e
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-05-20 17:03:15 -0300

    clase 11 añadida

---
+++ Clase11/Clase11.java
@@ -0,0 +1,10 @@
+package Clase11;
+
+//! Ejemplo bucle FOR
+public class Clase11 {
+  public static void main(String[] args) {
+    for (int cont = 0; cont <= 10; cont++) {
+      System.out.println("Estoy dando vueltas con for, estoy en la vuelta: " + cont);
+    }
+  }
+}
