dc78d56509342afe2a5ed1b76f32ee742ce0a583
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-05-31 17:57:42 -0300

    clase 14 actualizado

--- Clase14/Clase14.java
+++ Clase14/Clase14.java
@@ -3,4 +3,13 @@ package Clase14;
 //! Matrices
 
 public class Clase14 {
+  public static void main(String[] args) {
+    // Declaracion
+    int matriz[][] = new int[3][3];
+
+    // Asignacion (Manual)
+    matriz[0][0] = 5;
+    matriz[0][1] = 13;
+    matriz[2][2] = 24;
+  }
 }
